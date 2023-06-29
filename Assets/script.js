88892efe8eec006c17b729b9676c35514699ac3e
Cristofferb7 <136852733+Cristofferb7@users.noreply.github.com>
2023-06-29 16:22:36 -0400

    added java.script fixes and fixed the function for password, added arrays and made generate password

--- Assets/script.js
+++ Assets/script.js
@@ -1,27 +1,81 @@
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var choiceArray= [];
+// added multiple arrays 
+var specialCharacterArray = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"] ;
+var lowerCaseArray = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseCharacterArray = [	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-// Write password to the #password input
-function writePassword() {
+
+// 1. promt for password criteria
+function getPrompts(){
+  choiceArray = [];
+  characterLenght = parseInt(prompt("how many characters"));
+
+  // 2.validate the input.
+  if(characterLenght< 8 || characterLenght > 128) {
+    alert("that number is not between 8 and 128. Please enter a correct number.");
+    return false; 
+
+}
+
+if (confirm ("would you like lowercase letters in your password?")) {
+choiceArray = choiceArray.concat(lowerCaseArray);
+}
+
+if (confirm ("would you like uppercase letters in your password?")) {
+choiceArray = choiceArray.concat(upperCaseCharacterArray);
+}
+
+if (confirm ("would you like special characters in your password?")) {
+choiceArray = choiceArray.concat(specialCharacterArray);
+}
+
+
+if (confirm ("would you like numbers in your password?")) {
+choiceArray = choiceArray.concat(numberArray);
+}
+
+return true;
+}
+
+
+  // 3. Generate password based on criteria.
+function writePassword(){
+  var correctPromts= getPrompts();
+
+  if(correctPromts){
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+}
+else{
+  passwordText.value = "";
+}
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 
-if(passObj.count < 8 | passObj.count > 128) {
-  alert("that number is not between 8 and 128. Please enter a correct number.");
+function generatePassword(){
 
-}
-  function buildPassword(){
+  var password = "";
+  for (let i = 0; i < characterLenght; i++) {
+    var randomLetter = Math.floor(Math.random() * choiceArray.length);
 
-
+    password = password + choiceArray[randomLetter];
+    
   }
+  return password;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
